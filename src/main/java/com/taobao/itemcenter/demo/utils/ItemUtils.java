7821c4b54e8b45be3adc8c94ebe1d2280e138550
tieyi.qlr@gmail.com <tieyi.qlr@gmail.com@15a32430-4c70-ded6-f7bf-8cd43194bee3>
2012-03-07 04:58:26 +0000

    增加查询商品工具类和查询接口的基本结构

---
+++ src/main/java/com/taobao/itemcenter/demo/utils/ItemUtils.java
@@ -0,0 +1,47 @@
+package com.taobao.itemcenter.demo.utils;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.taobao.item.constant.AppInfoConstants;
+import com.taobao.item.domain.AppInfoDO;
+import com.taobao.item.domain.ItemDO;
+import com.taobao.item.domain.query.QueryItemOptionsDO;
+import com.taobao.item.domain.result.ItemResultDO;
+import com.taobao.item.exception.IcException;
+import com.taobao.item.service.client.ItemQueryServiceClient;
+
+/**
+ * 商品工具类，可以用于获取宝贝信息
+ * @author tieyi.qlr
+ *
+ */
+public class ItemUtils {
+	
+	private static ItemQueryServiceClient itemQueryServiceClient;
+	
+	static{
+		ApplicationContext ac = new ClassPathXmlApplicationContext("itemQueryServiceClient/spirng-ic-hsf.xml");
+		itemQueryServiceClient = (ItemQueryServiceClient)ac.getBean("itemQueryServiceClient");
+	}
+	
+	public static ItemDO getSimpleItem(long itemId){
+		QueryItemOptionsDO options = new QueryItemOptionsDO();
+		AppInfoDO app = new AppInfoDO(AppInfoConstants.NAME_DETAIL, "query", "detail");
+		try {
+			ItemResultDO result = itemQueryServiceClient.queryItemById(itemId, options, app);
+			if(result.isSuccess()){
+				return result.getItem();
+			}
+		} catch (IcException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		ItemUtils.getSimpleItem(123);
+		System.out.println();
+	}
+}
